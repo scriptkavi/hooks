a7b64c9fec8e314b4bdca9022a3f2648d094481e
sunnynk19 <sunnynk19@gmail.com>
2024-07-18 21:14:21 +0530

    (fix) fixed footer logo

--- apps/www/components/site-footer.tsx
+++ apps/www/components/site-footer.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image"
 import Link from "next/link"
-import scriptkavilogo from "@/public/android-chrome-192x192.png"
 
 import { siteConfig } from "@/config/site"
+
+import { Icons } from "./icons"
 
 export function SiteFooter() {
   return (
     <footer className="py-6 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <div className="text-balance flex items-center space-x-2 text-center text-sm leading-loose text-muted-foreground md:text-left">
-          <Image src={scriptkavilogo} height={20} alt="ScriptKavi" />
+          <Icons.logo className="h-6 w-6 fill-primary" />
           <div>
             Crafted by{" "}
             <Link
